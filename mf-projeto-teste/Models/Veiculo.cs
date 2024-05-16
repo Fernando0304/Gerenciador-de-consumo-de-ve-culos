11c3ea8153729ce36c8d16b89fbc7019bda154c6
Fernando Marques e Marques <1487714@sga.pucminas.br>
2024-05-16 00:18:10 -0300

    Configaração do Banco de Dados e da Tabela veiculo

---
+++ mf-projeto-teste/Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_projeto_teste.Models
+{
+    [Table("Veiculos")]
+
+    public class Veiculo
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o nome")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Obrigatório informa a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o Ano de Fabricação")]
+        public int AnoFabricação { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
